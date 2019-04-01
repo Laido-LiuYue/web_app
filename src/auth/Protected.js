8c0a60c1fdaa140f35362796c1edab24ceef0cc8
Laido <578201842@qq.com>
2019-04-01 20:44:22 +0800

    Counter和TodoList

---
+++ src/auth/Protected.js
@@ -0,0 +1,5 @@
+import React from 'react'
+function Protected(){
+    return <h1>Protected</h1>
+}
+export default Protected;
---
+++ src/containers/Tubiao.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Tubiao extends React.Component {
+    render() {
+        return (
+            <div>
+                图表
+            </div>
+        )
+    }
+}
